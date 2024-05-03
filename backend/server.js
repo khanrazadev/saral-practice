e308d6e26955769aa369dc60daa0a657d4f4a576
raza <razakhanha05@gmail.com>
2024-05-04 04:48:09 +0530

    TEMP -FIX

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,22 @@ import cors from "cors";
 dotenv.config();
 
 const PORT = process.env.PORT || 3010;
+
 const app = express();
+// Middleware to set CORS headers
+app.use((req, res, next) => {
+  // Allow all origins
+  res.header("Access-Control-Allow-Origin", "*");
+  // Allow specific methods
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  // Allow specific headers
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  // Continue to the next middleware
+  next();
+});
 const corsOptions = {
   origin: "https://saral-practice.vercel.app",
 };
