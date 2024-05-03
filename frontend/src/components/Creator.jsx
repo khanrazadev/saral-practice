0e3ed4cef04ca2dbb0a4e8e759d4c84a16329044
raza <razakhanha05@gmail.com>
2024-05-04 04:14:59 +0530

    testing-deployment issue

--- frontend/src/components/Creator.jsx
+++ frontend/src/components/Creator.jsx
@@ -53,7 +53,7 @@ const Creator = ({ creator, status }) => {
       <div className="text-xs flex justify-around">
         <div className="flex justify-center items-center text-center gap-2">
           <img
-            src={`/external-images/${trimmedAvatar}`}
+            src={avatar}
             alt=""
             className="w-10 h-10 object-cover rounded-full"
           />
